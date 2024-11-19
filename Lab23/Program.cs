a1cd136cf92e6841500f8ee653e00f1f96157f2a
Павел Каплин <spike1321@yandex.ru>
2024-11-19 21:53:34 +0300

    Добавьте файлы проекта.

---
+++ Lab23/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Lab23
+{
+    class Program
+    {
+      
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число: ");
+            int f = int.Parse(Console.ReadLine());
+            FactorialAsync(f);
+            Console.ReadKey();
+         
+        }
+
+        static void Factorial(int a)
+        {
+            int count = 1;
+            for (int i = 1; i <= a; i++)
+            {
+                if (a == 0)
+                {
+                    count = 1;
+                }
+                else
+                count = count * i;
+                Thread.Sleep(100);
+            }
+            Console.WriteLine("Факториал равен: {0}", count);
+        }
+
+
+        static async void FactorialAsync(int a)
+        {
+            await Task.Run(()=>Factorial(a));
+        }
+    }
+}
